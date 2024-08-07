a540775c288cb658e81743a4058dd14aafc60b5f
bromeliakiller <160151963+bromeliakiller@users.noreply.github.com>
2024-08-07 10:12:52 -0300

    Create recomendado.py

---
+++ recomendado.py
@@ -0,0 +1,10 @@
+#recomendado
+depar = input()
+if depar == "Desenvolvimento de Software":
+    print("Laptops")
+if depar == "Marketing":
+    print("Tablets")
+if depar == "Recursos Humanos":
+    print("Desktop")
+if depar == "Pesquisa e Desenvolvimento":
+    print("equipamentos com especificações de última geração")
